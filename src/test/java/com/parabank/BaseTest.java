dd9a96e065ca703221144a09efac7aef9749efda
sdetposition <sdetposition@gmail.com>
2026-06-03 11:28:02 +0530

    Reports Added

--- src/test/java/com/parabank/BaseTest.java
+++ src/test/java/com/parabank/BaseTest.java
@@ -39,7 +39,7 @@ public class BaseTest {
 			prefs.put("credentials_enable_service", false);
 			prefs.put("profile.password_manager_enabled", false);
 			
-		//	options.addArguments("--headless=new");
+			options.addArguments("--headless=new");
 			options.addArguments("--window-size=1920,1080");
 			options.addArguments("--disable-gpu");
 
